d2c500b3672903004f0ae306b00472ac8bf1b9b9
Oduxt <93281983+Oduxt@users.noreply.github.com>
2021-10-28 13:00:05 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,14 @@
+def on_forever():
+    turtle.forward(4)
+    turtle.turn_right()
+    turtle.forward(1)
+    turtle.turn_right()
+    turtle.forward(4)
+    turtle.turn_left()
+    turtle.forward(1)
+    turtle.turn_left()
+basic.forever(on_forever)
+
+turtle.set_position(2, 2)
+turtle.turn_right()
+turtle.set_speed(20)
---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function on_forever() {
+    turtle.forward(4)
+    turtle.turnRight()
+    turtle.forward(1)
+    turtle.turnRight()
+    turtle.forward(4)
+    turtle.turnLeft()
+    turtle.forward(1)
+    turtle.turnLeft()
+})
+turtle.setPosition(2, 2)
+turtle.turnRight()
+turtle.setSpeed(20)
